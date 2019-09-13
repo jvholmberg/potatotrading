d676578d47ebb81c33fca5b14906d1e816c9eeb4
Johan Holmberg <johanholmberg@Johans-Mac.local>
2019-09-13 23:34:22 +0200

    Added dropdown skeleton

--- src/containers/Landing/LandingView.js
+++ src/containers/Landing/LandingView.js
@@ -2,7 +2,7 @@ import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-import { Button } from '../../components';
+import { Button, Dropdown } from '../../components';
 
 
 class LandingView extends PureComponent {
@@ -23,6 +23,11 @@ class LandingView extends PureComponent {
         <Button.Light>Light</Button.Light>
         <Button.Dark>Dark</Button.Dark>
         <Button.Link>Link</Button.Link>
+				<Dropdown>
+					<Dropdown.Text>
+						hej
+					</Dropdown.Text>
+				</Dropdown>
         Landing
       </Fragment>
     );

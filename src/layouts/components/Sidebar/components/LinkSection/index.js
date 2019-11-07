9631027728a8a25fcd5baaf02d1adbdb3fbd946f
Johan Holmberg <johanholmberg@Johans-Mac.local>
2019-11-07 22:27:44 +0100

    ...

---
+++ src/layouts/components/Sidebar/components/LinkSection/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { List, ListItem, Button, colors } from '@material-ui/core';
+
+const LinkSection = props => (
+	<span />
+);
+
+LinkSection.propTypes = {
+
+};
+
+export default LinkSection;

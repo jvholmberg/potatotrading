e7754950b1a4d5bf588e199013f99780839eb2c1
Johan Holmberg <johan@forkyfork.com>
2019-12-09 22:51:27 +0100

    Simplified ScreenSpy

--- src/components/ScreenSpy/index.js
+++ src/components/ScreenSpy/index.js
@@ -1,17 +1,10 @@
-import React, { PureComponent } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { useMediaQuery } from '@material-ui/core';
 import { useTheme } from '@material-ui/styles';
 import mapDispatchToProps from './selectors';
 
-/**
- * @param {boolean} xs -
- * @param {boolean} sm -
- * @param {boolean} md -
- * @param {boolean} lg -
- * @param {boolean} xl -
- */
 const getScreenSize = (xs, sm, md, lg, xl) => {
   if (xl) return 'xl';
   if (lg) return 'lg';
@@ -20,37 +13,6 @@ const getScreenSize = (xs, sm, md, lg, xl) => {
   if (xs) return 'xs';
   return 'xs';
 }
-
-class ScreenReporter extends PureComponent {
-  componentDidMount() {
-    this.handleReport();
-  }
-
-  componentDidUpdate() {
-    this.handleReport();
-  }
-
-  handleReport = () => {
-    const {
-      xs, sm, md, lg, xl, setScreenSize,
-    } = this.props;
-    const screenSize = getScreenSize(xs, sm, md, lg, xl);
-    return setScreenSize(screenSize);
-  }
-
-  render() {
-    return null;
-  }
-}
-
-ScreenReporter.propTypes = {
-  xs: PropTypes.bool.isRequired,
-  sm: PropTypes.bool.isRequired,
-  md: PropTypes.bool.isRequired,
-  lg: PropTypes.bool.isRequired,
-  xl: PropTypes.bool.isRequired,
-  setScreenSize: PropTypes.func.isRequired,
-};
 
 /**
  * Determine screen size and pass to ScreenReporter
@@ -65,11 +27,12 @@ const ScreenSpy = ({ setScreenSize }) => {
   const lg = useMediaQuery(theme.breakpoints.up('lg'));
   const xl = useMediaQuery(theme.breakpoints.up('xl'));
 
-  return (
-    <ScreenReporter {...{
-      xs, sm, md, lg, xl, setScreenSize,
-    }} />
-  );
+  useEffect(() => {
+    const screenSize = getScreenSize(xs, sm, md, lg, xl);
+    setScreenSize(screenSize);
+  });
+
+  return null;
 };
 
 ScreenSpy.propTypes = {

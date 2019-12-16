8ea4f8cf6de800d219343c6950bdfcefc483a41e
Johan Holmberg <johan@forkyfork.com>
2019-12-16 23:31:12 +0100

    ...

--- src/routes/Diary/index.js
+++ src/routes/Diary/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import SessionForm from './SessionForm';
-import NutritionForm from './NutritionForm';
 
 const Diary = () => (
   <>
     <SessionForm />
-    <NutritionForm />
   </>
 );
 

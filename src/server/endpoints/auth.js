ec5f824332cf45f6bfeb70a9b11231d60f37dd6e
Johan Holmberg <johan@forkyfork.com>
2020-02-02 21:00:03 +0100

    ...

--- src/server/endpoints/auth.js
+++ src/server/endpoints/auth.js
@@ -3,22 +3,22 @@
 module.exports = (app) => {
 
   app.post('/users/auth', (req, res) => {
-    const mockFile = require('./getJwt.json');
+    const mockFile = require('../../sagas/auth/__mocks__/getJwt.json');
     res.json(mockFile);
   });
 
   app.get('/users/auth/:refreshToken', (req, res) => {
-    const mockFile = require('./refreshJwt.json');
+    const mockFile = require('../../sagas/auth/__mocks__/refreshJwt.json');
     res.json(mockFile);
   });
 
   app.get('/users/auth', (req, res) => {
-    const mockFile = require('./validateJwt.json');
+    const mockFile = require('../../sagas/auth/__mocks__/validateJwt.json');
     res.json(mockFile);
   });
 
   app.delete('/users/auth', (req, res) => {
-    const mockFile = require('./destroyJwt.json');
+    const mockFile = require('../../sagas/auth/__mocks__/destroyJwt.json');
     res.json(mockFile);
   });
 }
--- src/server/endpoints/sessions.js
+++ src/server/endpoints/sessions.js
@@ -3,17 +3,17 @@
 module.exports = (app) => {
 
   app.post('/sessions', (req, res) => {
-    const mockFile = require('./createSession.json');
+    const mockFile = require('../../sagas/sessions/__mocks__/createSession.json');
     res.json(mockFile);
   });
 
   app.get('/sessions', (req, res) => {
-    const mockFile = require('./getSessions.json');
+    const mockFile = require('../../sagas/sessions/__mocks__/getSessions.json');
     res.json(mockFile);
   });
 
   app.get('/sessions/types', (req, res) => {
-    const mockFile = require('./getSessionTypes.json');
+    const mockFile = require('../../sagas/sessions/__mocks__/getSessionTypes.json');
     res.json(mockFile);
   });
 }

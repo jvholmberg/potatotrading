565be0012960ed37d686449f8271649e2c67b701
Johan Holmberg <johan@forkyfork.com>
2020-02-12 09:35:41 +0100

    ...

--- src/sagas/auth/workers.js
+++ src/sagas/auth/workers.js
@@ -13,7 +13,6 @@ import {
 import * as Api from '../../utils/api';
 
 export function* workerGetJwt(action) {
-  console.log('dsf');
   try {
     yield put({ type: createRequestAction(GET_JWT, PENDING) });
     const { data } = yield call(Api.instance, {

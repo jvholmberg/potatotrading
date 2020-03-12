7aa4429a2d906a8a06eb9933917a7ac800be5a2a
Johan Holmberg <johan@forkyfork.com>
2020-03-12 20:04:21 +0100

    Started adding tests for SessionTable

---
+++ src/routes/Diary/components/SessionTable/__tests__/SessionTable.selectors.test.js
@@ -0,0 +1,49 @@
+import { DESC } from '../../../../../sagas/constants';
+import {
+  GET_SESSIONS, GET_SESSION_TYPES,
+} from '../../../../../sagas/sessions/actions';
+import * as selectors from '../SessionTable.selectors';
+
+describe('routes/Diary/components/SessionTable.selectors.js', () => {
+  const getMockState = () => ({
+    sessions: {
+      sessions: {
+        byId: {
+          0: { timestamp: 2 },
+          1: { timestamp: 1 },
+          2: { timestamp: 3 },
+        },
+        allIds: [0, 1, 2],
+      },
+      sessionTypes: { byId: {}, allIds: [] },
+      requests: {
+        [GET_SESSIONS]: { pending: false, done: false, error: null },
+        [GET_SESSION_TYPES]: { pending: false, done: false, error: 'error' },
+      },
+    },
+    ui: {
+      sorting: {
+        diary: { key: 'timestamp', direction: DESC },
+      },
+    }
+  });
+
+  // Screen
+  it('selects fetching', () => {
+    const actual = selectors.selectIsFetching(getMockState());
+    const expected = false;
+    expect(actual).toEqual(expected);
+  });
+
+  it('selects error', () => {
+    const actual = selectors.selectFetchError(getMockState());
+    const expected = 'error';
+    expect(actual).toEqual(expected);
+  });
+
+  it('selects sorted session ids', () => {
+    const actual = selectors.selectSessionsIdsSorted(getMockState());
+    const expected = [2, 0, 1];
+    expect(actual).toEqual(expected);
+  });
+});
---
+++ src/routes/Diary/components/SessionTable/__tests__/SessionTable.test.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undefined */
+/* eslint-disable max-lines-per-function */
+import React from 'react';
+import { shallow } from 'enzyme';
+import * as sagaSelectors from '../../../../../sagas/ui/selectors';
+import * as componentSelectors from '../SessionTable.selectors';
+import { DESC } from '../../../../../sagas/constants';
+import SessionTable from '..';
+import SessionTableError from '../SessionTable.error';
+import SessionTableLoading from '../SessionTable.loading';
+import SessionTableEmpty from '../SessionTable.empty';
+import SessionTableIdeal from '../SessionTable.ideal';
+
+describe('<SessionTable />', () => {
+  it('should return error', () => {
+    jest.spyOn(componentSelectors, 'selectIsFetching').mockReturnValue(false);
+    jest.spyOn(componentSelectors, 'selectFetchError').mockReturnValue('error');
+    jest.spyOn(sagaSelectors, 'selectSortingDiary').mockReturnValue({ key: 'timestamp', direction: DESC });
+    jest.spyOn(componentSelectors, 'selectSessionsIdsSorted').mockReturnValue(undefined);
+    const wrapper = shallow(<SessionTable />);
+    expect(wrapper.find(SessionTableError)).toExist();
+    expect(wrapper.find(SessionTableLoading)).not.toExist();
+    expect(wrapper.find(SessionTableEmpty)).not.toExist();
+    expect(wrapper.find(SessionTableIdeal)).not.toExist();
+  });
+
+  it('should return pending', () => {
+    jest.spyOn(componentSelectors, 'selectIsFetching').mockReturnValue(true);
+    jest.spyOn(componentSelectors, 'selectFetchError').mockReturnValue(null);
+    jest.spyOn(sagaSelectors, 'selectSortingDiary').mockReturnValue({ key: 'timestamp', direction: DESC });
+    jest.spyOn(componentSelectors, 'selectSessionsIdsSorted').mockReturnValue(undefined);
+    const wrapper = shallow(<SessionTable />);
+    expect(wrapper.find(SessionTableError)).not.toExist();
+    expect(wrapper.find(SessionTableLoading)).toExist();
+    expect(wrapper.find(SessionTableEmpty)).not.toExist();
+    expect(wrapper.find(SessionTableIdeal)).not.toExist();
+    expect(wrapper.find(SessionTableIdeal)).not.toExist();
+  });
+
+  it('should return empty', () => {
+    jest.spyOn(componentSelectors, 'selectIsFetching').mockReturnValue(false);
+    jest.spyOn(componentSelectors, 'selectFetchError').mockReturnValue(null);
+    jest.spyOn(sagaSelectors, 'selectSortingDiary').mockReturnValue({ key: 'timestamp', direction: DESC });
+    jest.spyOn(componentSelectors, 'selectSessionsIdsSorted').mockReturnValue([]);
+    const wrapper = shallow(<SessionTable />);
+    expect(wrapper.find(SessionTableError)).not.toExist();
+    expect(wrapper.find(SessionTableLoading)).not.toExist();
+    expect(wrapper.find(SessionTableEmpty)).toExist();
+    expect(wrapper.find(SessionTableIdeal)).not.toExist();
+  });
+
+  it('should return ideal', () => {
+    jest.spyOn(componentSelectors, 'selectIsFetching').mockReturnValue(false);
+    jest.spyOn(componentSelectors, 'selectFetchError').mockReturnValue(null);
+    jest.spyOn(sagaSelectors, 'selectSortingDiary').mockReturnValue({ key: 'timestamp', direction: DESC });
+    jest.spyOn(componentSelectors, 'selectSessionsIdsSorted').mockReturnValue([0, 1, 2, 3]);
+    const wrapper = shallow(<SessionTable />);
+    expect(wrapper.find(SessionTableError)).not.toExist();
+    expect(wrapper.find(SessionTableLoading)).not.toExist();
+    expect(wrapper.find(SessionTableEmpty)).not.toExist();
+    expect(wrapper.find(SessionTableIdeal)).toExist();
+  });
+});
